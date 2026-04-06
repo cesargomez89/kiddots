2c42d84748ba014f0b10443b018f293b64c8fec1
Cesar Gomez <cesargomez89@gmail.com>
2026-04-06 13:00:57 -0600

    feat: add copilot to blink

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -6,16 +6,32 @@ return {
 		"rafamadriz/friendly-snippets",
 		"Huijiro/blink-cmp-supermaven",
 		"milanglacier/minuet-ai.nvim",
+		"giuxtaposition/blink-cmp-copilot",
 	},
 	opts = {
 		snippets = { preset = "luasnip" },
 		sources = {
-			default = { "minuet", "supermaven", "lsp", "snippets", "buffer", "path" },
+			default = { "minuet", "copilot", "supermaven", "lsp", "snippets", "buffer", "path" },
 			providers = {
 				supermaven = {
 					name = "Supermaven",
 					module = "blink-cmp-supermaven",
 					score_offset = 100,
+				},
+				copilot = {
+					name = "copilot",
+					module = "blink-cmp-copilot",
+					score_offset = 100,
+					async = true,
+					transform_items = function(_, items)
+						local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+						local kind_idx = #CompletionItemKind + 1
+						CompletionItemKind[kind_idx] = "Copilot"
+						for _, item in ipairs(items) do
+							item.kind = kind_idx
+						end
+						return items
+					end,
 				},
 				minuet = {
 					name = "Minuet",
@@ -38,6 +54,42 @@ return {
 			["<S-Tab>"] = { "select_prev", "fallback" },
 			["<C-b>"] = { "scroll_documentation_up", "fallback" },
 			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+		},
+		appearance = {
+			-- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
+			kind_icons = {
+				Copilot = "",
+				Text = "󰉿",
+				Method = "󰊕",
+				Function = "󰊕",
+				Constructor = "󰒓",
+
+				Field = "󰜢",
+				Variable = "󰆦",
+				Property = "󰖷",
+
+				Class = "󱡠",
+				Interface = "󱡠",
+				Struct = "󱡠",
+				Module = "󰅩",
+
+				Unit = "󰪚",
+				Value = "󰦨",
+				Enum = "󰦨",
+				EnumMember = "󰦨",
+
+				Keyword = "󰻾",
+				Constant = "󰏿",
+
+				Snippet = "󱄽",
+				Color = "󰏘",
+				File = "󰈔",
+				Reference = "󰬲",
+				Folder = "󰉋",
+				Event = "󱐋",
+				Operator = "󰪚",
+				TypeParameter = "󰬛",
+			},
 		},
 	},
 	config = function(_, opts)
