23908ec99c0cd9d67e25018bc18bff2d8cf42799
Cesar Gomez <cesargomez89@gmail.com>
2026-04-06 13:48:53 -0600

    fix: completions

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -10,35 +10,32 @@ return {
 	},
 	opts = {
 		snippets = { preset = "luasnip" },
+		completion = {
+			ghost_text = {
+				enabled = true,
+			},
+		},
 		sources = {
-			default = { "minuet", "copilot", "supermaven", "lsp", "snippets", "buffer", "path" },
+			default = { "copilot", "supermaven", "minuet", "lsp", "snippets", "buffer", "path" },
 			providers = {
-				supermaven = {
-					name = "Supermaven",
-					module = "blink-cmp-supermaven",
-					score_offset = 100,
-				},
 				copilot = {
 					name = "copilot",
 					module = "blink-cmp-copilot",
 					score_offset = 100,
 					async = true,
-					transform_items = function(_, items)
-						local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-						local kind_idx = #CompletionItemKind + 1
-						CompletionItemKind[kind_idx] = "Copilot"
-						for _, item in ipairs(items) do
-							item.kind = kind_idx
-						end
-						return items
-					end,
+				},
+				supermaven = {
+					name = "supermaven",
+					module = "blink-cmp-supermaven",
+					score_offset = 80,
+					async = true,
 				},
 				minuet = {
-					name = "Minuet",
+					name = "minuet",
 					module = "minuet.blink",
-					score_offset = 90,
-					async = true,
 					timeout_ms = 500,
+					async = true,
+					score_offset = 50,
 					enabled = function()
 						local handle = io.popen("nc -z localhost 8081 > /dev/null 2>&1 && echo 'up' || echo 'down'")
 						local res = handle:read("*a"):gsub("%s+", "")
@@ -54,42 +51,6 @@ return {
 			["<S-Tab>"] = { "select_prev", "fallback" },
 			["<C-b>"] = { "scroll_documentation_up", "fallback" },
 			["<C-f>"] = { "scroll_documentation_down", "fallback" },
-		},
-		appearance = {
-			-- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
-			kind_icons = {
-				Copilot = "",
-				Text = "󰉿",
-				Method = "󰊕",
-				Function = "󰊕",
-				Constructor = "󰒓",
-
-				Field = "󰜢",
-				Variable = "󰆦",
-				Property = "󰖷",
-
-				Class = "󱡠",
-				Interface = "󱡠",
-				Struct = "󱡠",
-				Module = "󰅩",
-
-				Unit = "󰪚",
-				Value = "󰦨",
-				Enum = "󰦨",
-				EnumMember = "󰦨",
-
-				Keyword = "󰻾",
-				Constant = "󰏿",
-
-				Snippet = "󱄽",
-				Color = "󰏘",
-				File = "󰈔",
-				Reference = "󰬲",
-				Folder = "󰉋",
-				Event = "󱐋",
-				Operator = "󰪚",
-				TypeParameter = "󰬛",
-			},
 		},
 	},
 	config = function(_, opts)
