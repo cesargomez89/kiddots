33fb822bc23d58bc71b05aa7f59341a8a9bebf4f
Cesar Gomez <cesargomez89@gmail.com>
2026-04-21 16:08:44 -0600

    feat: improve agent-serve command

--- zsh/functions/llama-serve.zsh
+++ zsh/functions/llama-serve.zsh
@@ -17,19 +17,31 @@ function completion-serve() {
 }
 
 function agent-serve() {
-  local model="unsloth/gemma-4-26B-A4B-it-GGUF:UD-Q4_K_XL"
+  local model="unsloth/Qwen3.6-35B-A3B-GGUF:UD-Q4_K_M"
   local port="${1:-8082}"
+
+  ulimit -l unlimited
 
   llama-server \
     -hf "$model" \
     --port "$port" \
-    # -ngl 80 \
-    -c 131072 \
-    -n 16384 \
+    --no-mmproj \
+    --fit on \
+    --fit-ctx 32768 \
+    --fit-target 1200 \
+    --n-cpu-moe 17 \
+    -np 1 \
     -fa on \
-    -ctk q4_0 \
-    -ctv q4_0 \
+    --no-mmap \
+    --mlock \
     -b 2048 \
-    -ub 512 \
-    --temp 0.3 --top-k 20 --top-p 0.90 --min-p 0.1
+    -ub 1024 \
+    -ctk q8_0 \
+    -ctv q8_0 \
+    -t 8 \
+    --temp 0.6 \
+    --top-p 0.95 \
+    --min-p 0.05 \
+    --reasoning-budget -1 \
+    --chat-template-kwargs "{\"preserve_thinking\": true}"
 }
