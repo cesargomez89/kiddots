88c1f1d664ff0beaa9089af27ceafaee01058f06
Cesar Gomez <cesargomez89@gmail.com>
2026-05-05 09:34:47 -0600

    chore: testing llama server

--- zsh/functions/llama-serve.zsh
+++ zsh/functions/llama-serve.zsh
@@ -20,28 +20,29 @@ function agent-serve() {
   local model="unsloth/Qwen3.6-35B-A3B-GGUF:UD-Q4_K_M"
   local port="${1:-8082}"
 
-  ulimit -l unlimited
-
   llama-server \
     -hf "$model" \
     --port "$port" \
-    --no-mmproj \
     --fit on \
-    --fit-ctx 32768 \
-    --fit-target 1200 \
-    --n-cpu-moe 17 \
+    --fit-ctx 65536 \
+    --fit-target 1300 \
+    --no-mmproj \
+    --n-cpu-moe 18 \
+    --threads 12 \
     -np 1 \
     -fa on \
     --no-mmap \
     --mlock \
     -b 2048 \
-    -ub 1024 \
+    -ub 2048 \
     -ctk q8_0 \
     -ctv q8_0 \
-    -t 8 \
     --temp 0.6 \
     --top-p 0.95 \
-    --min-p 0.05 \
+    --top-k 20 \
+    --min-p 0.0 \
+    --presence-penalty 0.0 \
+    --repeat-penalty 1.0 \
     --reasoning-budget -1 \
-    --chat-template-kwargs "{\"preserve_thinking\": true}"
+    --chat-template-kwargs "{\"preserve_thinking\": true}" \
 }
